2a5be02284ff29b575312c07f2f791fcfcbd0312
abdulbarihtp <135722005+abdulbarihtp@users.noreply.github.com>
2023-07-04 19:09:02 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -52,5 +52,12 @@ Route::get('views_yosi/edit_yosi/{id}', 'App\Http\Controllers\ControllerYosi@edi
 Route::post('views_yosi/edit_proses_yosi', 'App\Http\Controllers\ControllerYosi@edit_proses_yosi');
 Route::get('views_yosi/delete_yosi/{id}', 'App\Http\Controllers\ControllerYosi@delete_yosi');
 
+Route::get('abari_akun', 'App\Http\Controllers\AbariAkunController@index');
+Route::get('abari_akun/tambah','App\Http\Controllers\AbariAkunController@tambah');
+Route::post('abari_akun/tambah_proses','App\Http\Controllers\AbariAkunController@tambah_proses');
+Route::get('abari_akun/edit/{id}','App\Http\Controllers\AbariAkunController@edit');
+Route::post('abari_akun/edit_proses','App\Http\Controllers\AbariAkunController@edit_proses');
+Route::get('abari_akun/delete/{id}','App\Http\Controllers\AbariAkunController@delete');
+
 require('galehludi.php');
 require('dika.php');
